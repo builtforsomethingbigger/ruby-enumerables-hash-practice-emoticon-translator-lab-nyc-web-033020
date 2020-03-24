a4c688df86f693caf41ff270c385d94d9851b265
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-24 01:24:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,8 @@ def load_library(path)
   result
 end
 
-def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  library = load_library(file_path)
+def get_japanese_emoticon(path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(path)
   if library["get_emoticon"].include?(english_emoticon)
     library["get_emoticon"][english_emoticon]
   else
@@ -20,8 +20,8 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-  library = load_library(file_path)
+def get_english_meaning(path = './lib/emoticons.yml', japanese_emoticon)
+  library = load_library(path)
   if library["get_meaning"].include?(japanese_emoticon)
     library["get_meaning"][japanese_emoticon]
   else
